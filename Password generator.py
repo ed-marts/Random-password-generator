203e6b0d5943303e15b6971651f80b020f2df43d
ed-marts <edmarts.edm@gmail.com>
2026-01-06 17:45:30 +0200

    Added JSON file saving and did final arrangements

--- Password generator.py
+++ Password generator.py
@@ -1,63 +1,81 @@
 import random
 import json
+import os
+
+FILE_NAME = "passwords.json"
+
+# Load or create JSON file
+if os.path.exists(FILE_NAME):
+    with open(FILE_NAME, "r") as file:
+        data = json.load(file)
+else:
+    data = {"passwords": []}
+
 while True:
     lowercase = "abcdefghijklmnopqrstuvwxyz"
     uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     numbers = "0123456789"
     symbols = "!@#$%^&*()-_=+[]{};:',.<>?/`~"
-    option = ""
-    json_pass = []
-    cycle = 0
 
-    option = input("\nOptions:\n 1. Create new password \n 2. Load new password \n --->  ")
-    allcharacters = ""
-    passwordlength = int(input("\nEnter the desired password length: "))
-    passwordtype = input("Select the password type: \n 1. Random \n 2. Custom \n ---> " )
+    option = input("\nOptions:\n 1. Create new password \n 2. Load saved passwords \n 3. Exit \n ---> ")
+
     if option == "1":
+        allcharacters = ""
+
+        passwordlength = int(input("\nEnter the desired password length: "))
+        passwordtype = input("Select the password type: \n 1. Random \n 2. Custom \n ---> ")
+
+        if passwordlength < 5:
+            print("Password must be at least 5 characters.")
+            continue
+
         if passwordtype == "1":
             allcharacters = lowercase + uppercase + numbers + symbols
 
         elif passwordtype == "2":
-
-            up = input("Should the password contain uppercase letters?: ")
-
+            up = input("Include uppercase letters? (Yes/No): ")
             if up.title() == "Yes":
-                allcharacters+= uppercase
-            elif up.title() == "No":
-                uppercase = ""
-            else:
-                print("Invalid input. Please enter 'Yes' or 'No'.")
+                allcharacters += uppercase
 
-            low = input("Should the password contain lowercase letters?: "  )
+            low = input("Include lowercase letters? (Yes/No): ")
             if low.title() == "Yes":
                 allcharacters += lowercase
-            elif low.title() == "No":
-                lowercase = ""
-            else:
-                print("Invalid input. Please enter 'Yes' or 'No'.")
 
-            num =input("Should the password contain numbers?: "  )
+            num = input("Include numbers? (Yes/No): ")
             if num.title() == "Yes":
                 allcharacters += numbers
-            elif num.title() == "No":
-                numbers = ""
-            else:
-                print("Invalid input. Please enter 'Yes' or 'No'.")
 
-        while True:
-            if passwordlength >= 5:
-                password = "".join(random.sample(allcharacters, passwordlength))
-                print("Generated password:", password)
-                
-            save = int(input("Do you wish to save the passowrd? \n 1. Yes  \n 2. No \n --->"))
-            if save == "1":
-                json_pass.append(password)
-                
-            elif passwordlength < 5:
-                print("Your passowrd should have a minimum length of 8 characters.")
-                cycle += 1
-            break
-   
+            sym = input("Include symbols? (Yes/No): ")
+            if sym.title() == "Yes":
+                allcharacters += symbols
+
+        if allcharacters == "":
+            print("You must select at least one character type.")
+            continue
+
+        password = "".join(random.sample(allcharacters, passwordlength))
+        print("\nGenerated password:", password)
+
+        save = input("\nDo you wish to save the password? (Yes/No): ")
+        if save.title() == "Yes":
+            data["passwords"].append(password)
+
+            with open(FILE_NAME, "w") as file:
+                json.dump(data, file, indent=4)
+
+            print("Password saved successfully!")
+
     elif option == "2":
+        if data["passwords"]:
+            print("\nSaved passwords:")
+            for i, p in enumerate(data["passwords"], start=1):
+                print(f"{i}. {p}")
+        else:
+            print("No saved passwords found.")
 
+    elif option == "3":
+        print("Goodbye!")
         break
+
+    else:
+        print("Invalid option. Try again.")
